fd98c5f1c7a5dc0f199b5aa1e0f5e2d893cd4204
M1ralai <atahan.dmrr@gmail.com>
2024-10-09 17:24:59 +0300

    Add files via upload

--- withdraw_money.c
+++ withdraw_money.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
 int main(){
     int i, n, coin, running, decision;
-    coin = 2314232;
+    char close;
+    coin = 800;
     running = 1;
     while (running){
         printf("You have %d coin.\n",coin);
@@ -11,17 +11,30 @@ int main(){
                 printf("You could withdraw %d, you could withdraw 10.000 and its multipliers.\n",10000*i);
             }
         }
+        if (i == 1 || i == 0){
+            printf("You don't have enough money to withdraw.");
+            break;
+        }
         printf("Choose 10.000 multiplier you want to withdraw.\n");
-        scanf("%d", &decision);
-        if (decision >=0){
-            if (decision < i){
-                coin -= decision*10000;
-                printf("You withdraw %d, now you have %d.\n", 10000*decision,coin);
-            } else{
-                printf("Please choose one of the valid number, you entered %d but you could choose below %d\n", decision,i);
+        if (scanf("%d", &decision) == 1){
+            if (decision >=0){
+                if (decision < i){
+                    coin -= decision*10000;
+                    printf("You withdraw %d, now you have %d.\n", 10000*decision,coin);
+                } else{
+                    printf("Please choose one of the valid number, you entered %d but you could choose equal and below %d\n", decision,i);
+                }
+            } else {
+                printf("Choose positive number.\n");
             }
-        } else {
-            printf("Choose positive number.\n");
+        } else{
+            printf("You entered char, please write y/Y to close this app\n");
+            scanf("%c",&close);
+            if (close == 'y' || close == 'Y'){
+                return 1;
+            } else{
+                printf("You continue this app.\n");
+            }
         }
     }
 }
